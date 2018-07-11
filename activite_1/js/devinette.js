d6d2ea756a265bd9955366cdd6eb66e12232927e
kevinsinjy <40633341+kevinsinjy@users.noreply.github.com>
2018-07-11 10:11:04 +0200

    Update devinette.js

--- activite_1/js/devinette.js
+++ activite_1/js/devinette.js
@@ -11,26 +11,9 @@ console.log("Bienvenue dans ce jeu de devinette !");
 var solution = Math.floor(Math.random() * 100) + 1;
 
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
-console.log("(La solution est " + solution + ")");
+//console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
-
-/*
-switch (solution) {
-
-    case i>solution:
-        console.log('le chiffre est trop grand');
-        break;
-
-    case i<solution:
-        console.log('le chiffre est trop petit');
-        break;
-    
-    case i==solution:
-        console.log('BRAVO!');
-        break;
-}
-*/
 
 var numClick = 0;
 var essai = 6;
